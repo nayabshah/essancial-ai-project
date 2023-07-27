9c5754b9c069340d8961b90bb01f8ff494579800
ariba <ariba@smartgeeks.in>
2023-07-27 19:52:38 +0530

    project completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,87 @@
+import { useState } from "react";
+import axios from "axios";
+import Stonk from "./Stonk";
+import DatePicker from "react-datepicker";
+
+import "./App.css";
+import "react-datepicker/dist/react-datepicker.css";
 function App() {
-	return (
-		<h1>Hello world</h1>
-	);
+  const [fetchedData, setFetchedData] = useState(null);
+  const [startDate, setStartDate] = useState(new Date());
+  const [ticker, setTicker] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const year = startDate.getFullYear();
+    const month = String(startDate.getMonth() + 1).padStart(2, "0");
+    const day = String(startDate.getDate()).padStart(2, "0");
+
+    // Format the date string
+    const formattedDate = `${year}-${month}-${day}`;
+
+    try {
+      const res = await axios.post("/api/fetchStockData", {
+        ticker: ticker,
+        date: formattedDate,
+      });
+
+      setFetchedData(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <div className="flex justify-center align-center mt-4">
+        {fetchedData && fetchedData.length > 0 && (
+          <Stonk initialData={fetchedData} />
+        )}
+
+        <form className="bg-white  px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="username"
+            >
+              Symbol of stock
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              value={ticker}
+              onChange={(e) => setTicker(e.target.value)}
+              placeholder="Symbol"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Date
+            </label>
+
+            <DatePicker
+              selected={startDate}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              onChange={(date) => setStartDate(date)}
+              dateFormat="yyyy-MM-dd"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Get Data
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- server/app.js
+++ server/app.js
@@ -1,35 +1,84 @@
 // DO NOT MODIFY ANYTHING HERE, THE PLACE WHERE YOU NEED TO WRITE CODE IS MARKED CLEARLY BELOW
 
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
+const path = require("path");
 
 const app = express();
 
 app.use(function (req, res, next) {
-    const allowedOrigins = ['http://localhost:3000'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-credentials", true);
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-    next();
+  const allowedOrigins = ["http://localhost:3000"];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
 });
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(bodyParser.json());
 
-app.enable('trust proxy');
+app.enable("trust proxy");
 
-app.post('/api/fetchStockData', (req, res) => {
-    // YOUR CODE GOES HERE, PLEASE DO NOT EDIT ANYTHING OUTSIDE THIS FUNCTION
-    res.sendStatus(200);
+app.post("/api/fetchStockData", (req, res) => {
+  // YOUR CODE GOES HERE, PLEASE DO NOT EDIT ANYTHING OUTSIDE THIS FUNCTION
+  const { ticker, date } = req.body;
+  let config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${process.env.POLYGON_API_URL}/aggs/ticker/${ticker}/range/5/minute/${date}/${date}?adjusted=true&sort=asc&limit=120`,
+    headers: {
+      Authorization: `Bearer ${process.env.POLYGON_API_KEY}`,
+    },
+  };
+
+  axios
+    .request(config)
+    .then((response) => {
+      const newRes = response.data.results.map((item) => {
+        const date = new Date(item.t);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, "0");
+        const day = String(date.getDate()).padStart(2, "0");
+        const hours = String(date.getHours()).padStart(2, "0");
+        const minutes = String(date.getMinutes()).padStart(2, "0");
+        const seconds = String(date.getSeconds()).padStart(2, "0");
+
+        const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+        return {
+          date: formattedDate,
+          open: item.o,
+          close: item.c,
+          high: item.h,
+          low: item.l,
+          label: item.v,
+        };
+      });
+      res.status(200).json(newRes);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "../", "client", "build", "index.html"))
+);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
